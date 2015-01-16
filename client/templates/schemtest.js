89866d69bff28a3ff0b0a9bc198d639d8756ac81
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-01-16 10:32:47 -0800

    dynamic templating

---
+++ client/templates/schemtest.js
@@ -0,0 +1,28 @@
+// a helper helper ... DRY chapter fetching
+function getChapter() {
+    var c = Chapters.findOne();
+        if (c == undefined) return c;
+
+    //  sort contents by start
+    c.contents = _.sortBy(c.contents, function(r){
+        return r.start;
+    });
+
+    // calculate chapter length on the fly
+    c.length = _.max(c.contents, function(r){return r.end}).end;
+
+    // join references with resource records
+    _.each(c.contents, function(e,i,l){
+        var r = Resources.findOne(e.resource_id);
+        l[i] = _.extend(l[i], r)
+    });
+
+    return c;
+}
+
+Template.schemtest.helpers({
+    chapter: getChapter,
+});
+
+// global vars for displaying preview on click
+previewContent = undefined;
