8c3384b5b389c499fda6740264ba9aca03251891
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-04-05 15:01:27 -0700

    chapter reader includes a "next" menu

--- client/templates/read.js
+++ client/templates/read.js
@@ -2,4 +2,35 @@ Template.read.helpers({
     resource: function(){
         return Resources.findOne({"name": /bar tattoo/});
     },
+    chapter: function(){
+        var _id = this.toString();
+        console.log(_id)
+        return Chapters.findOne(_id);
+    },
+    paras: function(text){
+        if (text){
+            var paras = text.split(/(?:\s*\r\n|\s*\n|\s*\r){2,}/gm);
+            // paras = _.map(paras, function(p){
+            //     var words = p.split(/\s+/);
+            //     return words;
+            // })
+            paras = _.map(paras, function(p){
+                    return {
+                        text: p,
+                        length: p.split(/\s+/).length
+                    }
+                });
+            _.reduce(paras, function(sum,cur){
+                    var cumsum = sum + cur.length;
+                    cur.end = cumsum;
+                    return cumsum;
+                },0)
+                ;
+            return paras
+        }
+    },
+});
+
+Template.read.onRendered(function() {
+    this.$('.rebook-chapter').animate({top: 0}, 1000);
 });
