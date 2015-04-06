da4863d06fb845773242c87772297dfb49fcd662
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-04-05 22:40:20 -0700

    load chapters in a sequence programmatically, and resources in track 0

---
+++ client/templates/reader/readChapters.js
@@ -0,0 +1,36 @@
+Template.readChapters.helpers({
+    mainTrack: function(){
+        return _.where(this.contents, {track: 0})
+    },
+
+    resource: function(){
+        return Resources.findOne({_id: this.resource_id});
+    },
+
+    isText: function(){
+        return this.type == "text";
+    },
+
+    paras: function(text){
+        if (text){
+            var paras = text.split(/(?:\s*\r\n|\s*\n|\s*\r){2,}/gm);
+            paras = _.map(paras, function(p){
+                return {
+                    text: p,
+                    length: p.split(/\s+/).length
+                }
+            });
+            _.reduce(paras, function(sum,cur){
+                    var cumsum = sum + cur.length;
+                    cur.end = cumsum;
+                    return cumsum;
+                },0)
+                ;
+            return paras;
+        }
+    }
+});
+
+Template.readChapters.onRendered(function() {
+    // $('.rebook-chapter').animate({top: 0}, 1000);
+});
---
+++ client/templates/reader/readSequences.js
@@ -0,0 +1,5 @@
+Template.readSequences.helpers({
+	getChapter: function () {
+		return getChapter(this._id);
+	}
+});
