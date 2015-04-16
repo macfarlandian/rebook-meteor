5ac0bbc44d34b0578c4cefa0a3bd4f5fdb12019c
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-04-15 22:48:17 -0700

    load and display dummy data in the bottom menu

--- client/templates/reader/readBook.js
+++ client/templates/reader/readBook.js
@@ -98,5 +98,67 @@ Template.readBook.onRendered(function(){
 			$('.ui.sidebar.visible').sidebar('hide');
 		}
 		lastScroll = newscroll;
-	})
+	});
+
+	// progress bar stuff
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(10, "%");
+
+    var svg = d3.select(".ui.sidebar.progress svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+	    .append("g")
+	        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("/wordcount.tsv", type, function(error, data) {
+      console.log(data)
+      x.domain(data.map(function(d) { return d.chapter; }));
+      y.domain([0, d3.max(data, function(d) { return d.wordcount; })]);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Frequency");
+
+      svg.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.chapter); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.wordcount); })
+          .attr("height", function(d) { return height - y(d.wordcount); });
+
+    });
+
+    function type(d) {
+      d.wordcount = +d.wordcount;
+      return d;
+    }
 });
