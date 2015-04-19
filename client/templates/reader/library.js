211a9fd92883a8b8654930a00bc6e018ba40a4d9
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-04-18 20:07:14 -0700

    scale "spine thickness" by word count, fill in reading progress

--- client/templates/reader/library.js
+++ client/templates/reader/library.js
@@ -4,9 +4,39 @@ Template.library.helpers({
 	},
 	linkParams: function(){
 		return {bookId: this._id}
+	},
+	spineWidth: function(){
+		var width = spineWidth(this.wordcount);
+		width =  d3.max([width, 50]); // min width is 50
+		return "min-height: " + width + "px;";
+	},
+	formatWordcount: function(){
+		return d3.format(',.0f')(this.wordcount)
+	},
+	readTime: function(){
+		return moment.duration(this.wordcount / 250, 'minutes').humanize();
+	},
+	progressHeight: function(){
+		var total = this.wordcount,
+			chapters = this.chapterLengths(),
+			path = ReadingPaths.findOne({userId: Session.get('userId'), book: this._id}),
+			read = _.reduce(chapters, function(memo, current){
+				if (path && _.includes(path.path, current._id)) {
+					return memo + current.wordcount;
+				} else {
+					return memo;
+				}
+			}, 0)
+			;
+		return "height: " + read / total * 100 + "%;";
 	}
 });
 
 Template.library.onRendered(function(){
 	$('body').removeClass();
 });
+
+var spineWidth = d3.scale.linear()
+	.domain([0,375])
+	.range([0,1])
+	;
