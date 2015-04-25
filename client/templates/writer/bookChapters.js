adf52f44926b9df4c57636e65f425ac27aedd3fe
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-04-25 15:02:25 -0700

    different look for chapter groups in used chapterlist

--- client/templates/writer/bookChapters.js
+++ client/templates/writer/bookChapters.js
@@ -2,6 +2,11 @@ Template.bookChapters.helpers({
 	parentName: function(){
 		var parent = Containers.findOne({'contents._id': this._id});
 		if (parent) return parent.name;
+	},
+	firstOfGroup: function(){
+		var parent = Containers.findOne({'contents._id': this._id});
+		if (_.findIndex(parent.contents, {_id: this._id}) == 0) return true;
+		return false;
 	}
 });
 
