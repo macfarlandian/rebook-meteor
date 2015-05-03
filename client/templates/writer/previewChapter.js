f6f01367942c87f2bee54c93b91db8275b771a2a
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-05-03 14:00:27 -0700

    show audio and images in creator preview

--- client/templates/writer/previewChapter.js
+++ client/templates/writer/previewChapter.js
@@ -2,10 +2,24 @@ Template.previewChapter.helpers({
 	mainTrack: function(){
         return _.where(this.contents, {track: 0})
     },
+    trackOverlap: function(contents){
+        var para = this;
+        var otherTracks = _.filter(contents, function(item){
+            return item.track > 0;
+        });
+        return _.filter(otherTracks, function(item){
+            return (item.start >= para.end - para.length) && (item.start <= para.end);
+        });
+    },
     isText: function(){
         return _.includes(['text', 'markdown', 'text/markdown'], this.type);
     },
-
+    isImage: function(){
+        return this.type == "image";
+    },
+    isAudio: function(){
+        return this.type == "audio";
+    },
     paras: function(text){
         if (text){
             var paras = text.split(/(?:\s*\r\n|\s*\n|\s*\r){2,}/gm);
